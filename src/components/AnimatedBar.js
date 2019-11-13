d67179355d1dec428e9a981e8833f7a66b06e766
Efimenko Dmitriy <montifick77@gmail.com>
2019-11-13 10:31:55 +0200

    init

---
+++ src/components/AnimatedBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class AnimatedBar extends Component {
+  render() {
+    let widthStyle = { width: this.props.value };
+    return <View style={[styles.bar, widthStyle]} />;
+  }
+}
+
+const styles = {
+  bar: {
+    height: 15,
+    borderWidth: 1,
+    borderColor: '#c72f06',
+    backgroundColor: '#e75832',
+  },
+};
